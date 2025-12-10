28a2d977144ac65dae0b3a517069f9c973893774
Erik <erikmuar91@gmail.com>
2025-12-10 20:12:35 +0100

    Definitivo

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
 basic.forever(function () {
+
+    basic.showNumber(input.temperature())
+    if (input.temperature() > 22) {
+        basic.showString("calor")
+    } else {
+        basic.showString("fred")
+    }
+})
+basic.forever(function () {
     if (input.lightLevel() > 200) {
         music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
     } else if (input.lightLevel() < 50) {
@@ -6,4 +15,5 @@ basic.forever(function () {
     } else {
     	
     }
+
 })
