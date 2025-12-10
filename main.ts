73a334aa9a2856bd6be9c36356e8c410c4399b57
Erik <erikmuar91@gmail.com>
2025-12-10 18:43:25 +0100

    Repte 5 Música

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 basic.forever(function () {
-    basic.showNumber(input.temperature())
-    if (input.temperature() > 22) {
-        basic.showString("calor")
+    if (input.lightLevel() > 200) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.UntilDone)
+    } else if (input.lightLevel() < 50) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.yawn), music.PlaybackMode.UntilDone)
     } else {
-        basic.showString("fred")
+    	
     }
 })
